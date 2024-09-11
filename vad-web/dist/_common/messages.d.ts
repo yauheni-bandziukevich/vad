908b092f1f482c7fbc3a21f93cbd3ea8c7ab49f9
Manuel Salinardi <manuel.salinardi@icona.it>
2024-09-11 16:05:00 +0200

    Build test

--- vad-web/dist/_common/messages.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum Message {
-    AudioFrame = "AUDIO_FRAME",
-    SpeechStart = "SPEECH_START",
-    VADMisfire = "VAD_MISFIRE",
-    SpeechEnd = "SPEECH_END",
-    SpeechStop = "SPEECH_STOP"
-}
-//# sourceMappingURL=messages.d.ts.map
--- vad-web/dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function audioFileToArray(audioFileData: Blob): Promise<{
-    audio: Float32Array;
-    sampleRate: number;
-}>;
-//# sourceMappingURL=utils.d.ts.map
